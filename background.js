23f1dc50dffe0b5fa07c722bcc69b41950014851
Manu <manuel.sanchez.naveira@gmail.com>
2021-05-23 19:31:57 +0200

    Check every few seconds if new anime entries are added to the DOM, if so then request its score to MyAnimeList.net

--- background.js
+++ background.js
@@ -2,18 +2,34 @@ chrome.webNavigation.onCompleted.addListener(function(tab) {
   this.ifCrunchyRollThenRequestScoresFromMyAnimeList(tab);
 });
 
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  chrome.storage.local.get(["tabId"], function(data) {
+    this.getAnimeTitlesPendingToGetStoreAndRequestScore(data.tabId);
+  });
+});
+
 function ifCrunchyRollThenRequestScoresFromMyAnimeList(tab) {
-  if (tab.frameId==0 && tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
+  if (tab && tab.frameId==0 && tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
     console.log("Crunchyroll - Get Anime score from MyAnimeList.net - Requesting scores...");
-    chrome.scripting.executeScript({
-      target: { tabId: tab.tabId },
-      function: getAnimeTitlesPendingToGetScore
-      },
-      (animeTitlesToId) => {
-        this.requestScoresFromMyAnimeList(animeTitlesToId, tab.tabId);
-      }
-    );
+    this.getAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
+    chrome.storage.local.set({ ["tabId"]: tab.tabId });
+    chrome.alarms.create("checkCrunchyrollNewAnime", {delayInMinutes: 0.1, periodInMinutes: 0.1});
   }
+}
+
+function getAnimeTitlesPendingToGetStoreAndRequestScore(tabId) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    function: getAnimeTitlesPendingToGetScore
+    },
+    (animeTitlesToId) => {
+      if (chrome.runtime.lastError) {
+        chrome.alarms.clearAll();
+      } else {
+        this.requestScoresFromMyAnimeList(animeTitlesToId, tabId);
+      }
+    }
+  );
 }
 
 function requestScoresFromMyAnimeList(animeTitlesToId, tabId) {
@@ -24,10 +40,10 @@ function requestScoresFromMyAnimeList(animeTitlesToId, tabId) {
       .then(result => {
         if (result) {
           var score = /Scored (\d\.\d+)/g.exec(result)[1];
-          console.log(animeTitle + ' ' + score);
+          console.log(animeTitle + " " + score);
           chrome.storage.local.set({ [animeTitle]: { animeId, score } });
         } else {
-          chrome.storage.local.set({ [animeTitle]: { animeId, score: '?' } });
+          chrome.storage.local.set({ [animeTitle]: { animeId, score: "?" } });
         }
         chrome.scripting.executeScript({
           target: { tabId: tabId },
@@ -39,13 +55,13 @@ function requestScoresFromMyAnimeList(animeTitlesToId, tabId) {
 }
 
 
-// Functions called from executeScript:
+// Functions called from activeTab:
 
 function getAnimeTitlesPendingToGetScore() {
   var animeTitlesToId = {};
   var animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
   for (var i = 0; i < animeEntries.length; i++) {
-    if (animeEntries[i].parentElement.children[2].textContent.indexOf('| Score: ') === -1) {
+    if (animeEntries[i].parentElement.children[2].textContent.indexOf("| Score: ") === -1) {
       animeTitlesToId[animeEntries[i].textContent] = animeEntries[i].parentElement.parentElement.parentElement.id;
     }
   }
@@ -58,7 +74,7 @@ function setAnimeScore() {
     chrome.storage.local.get([animeEntries[i].textContent], function(animeScoreAndId) {
       var id = Object.entries(animeScoreAndId)[0][1].animeId;
       var score = Object.entries(animeScoreAndId)[0][1].score;
-      if (document.getElementById(id).children[0].children[0].children[2].textContent.indexOf('| Score: ') === -1) {
+      if (document.getElementById(id).children[0].children[0].children[2].textContent.indexOf("| Score: ") === -1) {
         document.getElementById(id).children[0].children[0].children[2].textContent = document.getElementById(id).children[0].children[0].children[2].textContent + "| Score: " + score;
       }
     });
