d656344c93748d652b8c58de9dc313fb0baac794
Manu <manuel.sanchez.naveira@gmail.com>
2021-05-23 16:24:31 +0200

    Get first score from MyAnimeList.net and print it out for each anime (pending to do more requests if more animes are loaded checking previously that do not have score)

--- background.js
+++ background.js
@@ -1,33 +1,53 @@
 function getAnimeTitles() {
-  var animeTitles = []
-  var animeEntries = document.getElementsByClassName("series-title");
+  var animeTitlesToId = {};
+  var animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
   for (var i = 0; i < animeEntries.length; i++) {
-    animeTitles.push(animeEntries[i].textContent);
+    animeTitlesToId[animeEntries[i].textContent] = animeEntries[i].parentElement.parentElement.parentElement.id;
+  }
+  return animeTitlesToId;
+}
+
+function setAnimeScore() {
+  var animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
+  for (var i = 0; i < animeEntries.length; i++) {
+    chrome.storage.local.get([animeEntries[i].textContent], function(animeScoreAndId) {
+      var title = Object.entries(animeScoreAndId)[0][0];
+      var id = Object.entries(animeScoreAndId)[0][1].animeId;
+      var score = Object.entries(animeScoreAndId)[0][1].score;
+      if (document.getElementById(id).children[0].children[0].children[2].textContent.indexOf('Score') === -1) {
+        document.getElementById(id).children[0].children[0].children[2].textContent = document.getElementById(id).children[0].children[0].children[2].textContent + "| Score: " + score;
+      }
+    });
   }
   return animeTitles;
 }
 
 chrome.webNavigation.onCompleted.addListener(function(tab) {
   if (tab.frameId==0 && tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
-      console.log("Crunchyroll load complete");
-      chrome.scripting.executeScript({
-        target: { tabId: tab.tabId },
-        function: getAnimeTitles
-    },
-    (animeTitles) => {
-      animeTitles[0].result.forEach(function(animeTitle) {
-        console.log(encodeURIComponent(animeTitle.trim()));
-        fetch('https://myanimelist.net/search/all?cat=all&q=' + encodeURIComponent(animeTitle.trim()))
-        .then(response => response.text())
-        .then(result => {
-          console.log(result);
-          //parse score
-          //save score associated in a map which key its the title
-          //invoke another function passing the map as argument
-          //https://stackoverflow.com/questions/17567624/pass-a-parameter-to-a-content-script-injected-using-chrome-tabs-executescript
-        });
-      });
-
-    });
+    console.log("Crunchyroll - Get Anime score from MyAnimeList.net - Requesting scores...");
+    chrome.scripting.executeScript({
+      target: { tabId: tab.tabId },
+      function: getAnimeTitles
+      },
+      (animeTitlesToId) => {
+        for (const [animeTitle, animeId] of Object.entries(animeTitlesToId[0].result)) {
+          fetch("https://myanimelist.net/search/all?cat=all&q=" + encodeURIComponent(animeTitle.trim()))
+          .then(response => response.text())
+          .then(result => {
+            if (result) {
+              var score = /Scored (\d\.\d+)/g.exec(result)[1];
+              console.log(animeTitle + ' ' + score);
+              chrome.storage.local.set({ [animeTitle]: { animeId, score } });
+            } else {
+              chrome.storage.local.set({ [animeTitle]: { animeId, score: '?' } });
+            }
+            chrome.scripting.executeScript({
+              target: { tabId: tab.tabId },
+              function: setAnimeScore
+            });
+          });
+        };
+      }
+    );
   }
 });
