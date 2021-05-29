46e04d4f1f9eba82a424b21b2a3cfde8541c59cf
Manu <manuel.sanchez.naveira@gmail.com>
2021-05-29 17:42:57 +0200

    Chrome local storage used as cache to avoid doing too many requests to MAL

--- background.js
+++ background.js
@@ -4,54 +4,64 @@ chrome.webNavigation.onCompleted.addListener(function(tab) {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   chrome.storage.local.get(["tabId"], function(data) {
-    this.getAnimeTitlesPendingToGetStoreAndRequestScore(data.tabId);
+    this.checkNewAnimeTitlesPendingToGetStoreAndRequestScore(data.tabId);
   });
 });
 
 function ifCrunchyRollThenRequestScoresFromMyAnimeList(tab) {
   if (tab && tab.frameId==0 && tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
     console.log("Crunchyroll - Get Anime score from MyAnimeList.net - Requesting scores...");
-    this.getAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
+    this.checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
     chrome.storage.local.set({ ["tabId"]: tab.tabId });
     chrome.alarms.create("checkCrunchyrollNewAnime", {delayInMinutes: 0.1, periodInMinutes: 0.1});
   }
 }
 
-function getAnimeTitlesPendingToGetStoreAndRequestScore(tabId) {
+function checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tabId) {
   chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    function: getAnimeTitlesPendingToGetScore
+      target: { tabId: tabId },
+      function: getAnimeTitlesPendingToGetScore
     },
     (animeTitlesToId) => {
       if (chrome.runtime.lastError) {
         chrome.alarms.clearAll();
       } else {
-        this.requestScoresFromMyAnimeList(animeTitlesToId, tabId);
+        this.requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId);
       }
     }
   );
 }
 
-function requestScoresFromMyAnimeList(animeTitlesToId, tabId) {
+function requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId) {
   if (animeTitlesToId && animeTitlesToId.length > 0 && animeTitlesToId[0].result) {
     for (const [animeTitle, animeId] of Object.entries(animeTitlesToId[0].result)) {
-      fetch("https://myanimelist.net/search/all?cat=all&q=" + encodeURIComponent(animeTitle.trim()))
-      .then(response => response.text())
-      .then(result => {
-        if (result) {
-          var score = /Scored (\d\.\d+)/g.exec(result)[1];
-          console.log(animeTitle + " " + score);
-          chrome.storage.local.set({ [animeTitle]: { animeId, score } });
-        } else {
-          chrome.storage.local.set({ [animeTitle]: { animeId, score: "?" } });
-        }
+      getDataFromLocalStorage(['animeTitle']).then(function(animeScoreAndId) {
+        var cachedScore = Object.entries(animeScoreAndId)[0][1].score;
         chrome.scripting.executeScript({
           target: { tabId: tabId },
-          function: setAnimeScore
+          function: showAnimeScore
         });
-      });
+      }).catch(this.requestScoreToMyAnimeListAndShowAnimeScore(animeTitle, animeId, tabId));
     };
   }
+}
+
+function requestScoreToMyAnimeListAndShowAnimeScore(animeTitle, animeId, tabId) {
+  fetch("https://myanimelist.net/search/all?cat=all&q=" + encodeURIComponent(animeTitle.trim()))
+  .then(response => response.text())
+  .then(result => {
+    if (result) {
+      var score = /Scored (\d\.\d+)/g.exec(result)[1];
+      console.log("[new] "  + animeTitle + " " + score);
+      chrome.storage.local.set({ [animeTitle]: { animeId, score } });
+    } else {
+      chrome.storage.local.set({ [animeTitle]: { animeId, score: "?" } });
+    }
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: showAnimeScore
+    });
+  });
 }
 
 
@@ -68,12 +78,13 @@ function getAnimeTitlesPendingToGetScore() {
       } else { // bug Crunchyroll: sometimes duplicates the anime entry, in this case then copy the score from the duplicated one
         animeEntries[i].parentElement.children[2].textContent = document.getElementById(id).children[0].children[0].children[2].textContent;
       }
+      break; // delete
     }
   }
   return animeTitlesToId;
 }
 
-function setAnimeScore() {
+function showAnimeScore() {
   var animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
   for (var i = 0; i < animeEntries.length; i++) {
     chrome.storage.local.get([animeEntries[i].textContent], function(animeScoreAndId) {
@@ -84,4 +95,17 @@ function setAnimeScore() {
       }
     });
   }
+}
+
+function getDataFromLocalStorage(sKey) {
+  return new Promise(function(resolve, reject) {
+    chrome.storage.local.get(sKey, function(items) {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        reject(chrome.runtime.lastError.message);
+      } else {
+        resolve(items[sKey]);
+      }
+    });
+  });
 }
