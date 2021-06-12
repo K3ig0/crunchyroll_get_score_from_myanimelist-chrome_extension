081ed6515d9437650e56c4e6638b609a012bd7b3
Manu <manuel.sanchez.naveira@gmail.com>
2021-06-12 18:33:27 +0200

    if MyAnimeList blocks for too many requests then do console.error

--- background.js
+++ background.js
@@ -37,7 +37,7 @@ function requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId) {
   if (animeTitlesToId && animeTitlesToId.length > 0 && animeTitlesToId[0].result) {
     for (const [animeTitle, animeId] of Object.entries(animeTitlesToId[0].result)) {
       chrome.storage.local.get([animeTitle], function(animeScoreAndId) {
-        if (animeScoreAndId && Object.entries(animeScoreAndId)[0] && Object.entries(animeScoreAndId)[0][1] && Object.entries(animeScoreAndId)[0][1].score !== '?') {
+        if (animeScoreAndId && Object.entries(animeScoreAndId)[0] && Object.entries(animeScoreAndId)[0][1] && Object.entries(animeScoreAndId)[0][1].score !== "?") {
           const cachedScore = Object.entries(animeScoreAndId)[0][1].score;
           console.debug("[cached] " + animeTitle + " " + cachedScore);
           chrome.scripting.executeScript({
@@ -54,25 +54,30 @@ function requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId) {
 
 function requestScoreToMyAnimeListAndShowAnimeScore(animeTitle, animeId, tabId) {
   fetch("https://myanimelist.net/search/all?cat=all&q=" + encodeURIComponent(animeTitle.trim()), {
-    mode: 'no-cors'
-  }).then(response => response.text())
-  .then(result => {
-    try {
-      const animeEntriesIndex = result.indexOf('<h2 id="anime">Anime</h2>');
-      const animeEntries = result.substring(animeEntriesIndex);
-      const scores = /Scored (\d\.\d+)/g.exec(animeEntries);
-      if (scores.length > 1) {
-        const score = scores[1];
-        console.debug("[new] "  + animeTitle + " " + score);
-        chrome.storage.local.set({ [animeTitle]: { animeId, score } });
-      }
-    } catch(ex) {
-      console.error(ex);
-      chrome.storage.local.set({ [animeTitle]: { animeId, score: "?" } });
+    mode: "no-cors"
+  }).then(response => {
+    if (response.status === 403) {
+      console.error("MyAnimeList.net access is temporarily disabled. Go to the site to enable it");
+      return;
     }
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: showAnimeScore
+    response.text().then(result => {
+      try {
+        const animeEntriesIndex = result.indexOf('<h2 id="anime">Anime</h2>');
+        const animeEntries = result.substring(animeEntriesIndex);
+        const scores = /Scored (\d\.\d+)/g.exec(animeEntries);
+        if (scores.length > 1) {
+          const score = scores[1];
+          console.debug("[new] "  + animeTitle + " " + score);
+          chrome.storage.local.set({ [animeTitle]: { animeId, score } });
+        }
+      } catch(ex) {
+        console.error(ex);
+        chrome.storage.local.set({ [animeTitle]: { animeId, score: "?" } });
+      }
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        function: showAnimeScore
+      });
     });
   });
 }
