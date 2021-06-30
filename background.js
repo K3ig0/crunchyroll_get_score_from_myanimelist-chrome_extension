2f7a0079e3f576b710471623ac01f86479cda9e7
Manu <manuel.sanchez.naveira@gmail.com>
2021-06-30 18:49:14 +0200

    release: 2.0.0 - beta.crunchyroll support and score in color like metacritic

--- background.js
+++ background.js
@@ -7,10 +7,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function ifCrunchyRollThenRequestScoresFromMyAnimeList(tab) {
-  if (tab && tab.frameId==0 && tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
-    this.createObserverOnDOM(tab.tabId);
-    this.checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
+  if (tab && tab.frameId==0) {
+    if (tab.url.indexOf("beta.crunchyroll.com") != -1) {
+      this.createObserverOnBetaDOM(tab.tabId);
+      this.checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
+    } else if (tab.url.indexOf("crunchyroll.com") != -1 && tab.url.indexOf("anime") != -1) {
+      this.createObserverOnDOM(tab.tabId);
+      this.checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tab.tabId);
+    }
   }
+}
+
+function createObserverOnBetaDOM(tabId) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    function: _createObserverOnBetaDOM
+  });
 }
 
 function createObserverOnDOM(tabId) {
@@ -26,9 +38,7 @@ function checkNewAnimeTitlesPendingToGetStoreAndRequestScore(tabId) {
       function: getAnimeTitlesPendingToGetScore
     },
     (animeTitlesToId) => {
-      if (!chrome.runtime.lastError) {
-        this.requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId);
-      }
+      this.requestScoresToMyAnimeListIfNotCachedAndShow(animeTitlesToId, tabId);
     }
   );
 }
@@ -100,6 +110,18 @@ function requestScoreToMyAnimeListAndShowAnimeScore(animeTitle, animeId, tabId) 
 
 // Functions called from activeTab:
 
+function _createObserverOnBetaDOM() {
+  const targetNode = document.querySelector(".erc-browse-cards-collection");
+  const config = { childList: true, subtree: true, attributes: false};
+  const callback = function(mutationsList, observer) {
+    if (mutationsList.length > 1) {
+        chrome.runtime.sendMessage({newAnimeEntries: mutationsList.length});
+    }
+  }
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+}
+
 function _createObserverOnDOM() {
     const targetNode = document.querySelector("#main_content");
     const config = { childList: true, subtree: true, attributes: false};
@@ -114,14 +136,25 @@ function _createObserverOnDOM() {
 
 function getAnimeTitlesPendingToGetScore() {
   const animeTitlesToId = {};
-  const animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
-  for (var i = 0; i < animeEntries.length; i++) {
-    if (animeEntries[i].parentElement.children[2].textContent.indexOf("| Score: ") === -1) {
-      const id = animeEntries[i].parentElement.parentElement.parentElement.id;
-      if (document.getElementById(id).children[0].children[0].children[2].textContent.indexOf("| Score: ") === -1) {
-        animeTitlesToId[animeEntries[i].textContent] = animeEntries[i].parentElement.parentElement.parentElement.id;
-      } else { // bug Crunchyroll: sometimes duplicates the anime entry, in this case then copy the score from the duplicated one
-        animeEntries[i].parentElement.children[2].textContent = document.getElementById(id).children[0].children[0].children[2].textContent;
+  const oldCrunchyRollMainContent = document.getElementById("main_content");
+  if (oldCrunchyRollMainContent) {
+    const animeEntries = oldCrunchyRollMainContent.getElementsByClassName("series-title");
+    for (var i = 0; i < animeEntries.length; i++) {
+      if (animeEntries[i].parentElement.children[2].textContent.indexOf("| Score: ") === -1) {
+        const id = animeEntries[i].parentElement.parentElement.parentElement.id;
+        if (document.getElementById(id).children[0].children[0].children[2].textContent.indexOf("| Score: ") === -1) {
+          animeTitlesToId[animeEntries[i].textContent] = animeEntries[i].parentElement.parentElement.parentElement.id;
+        } else { // bug Crunchyroll: sometimes duplicates the anime entry, in this case then copy the score from the duplicated one
+          animeEntries[i].parentElement.children[2].textContent = document.getElementById(id).children[0].children[0].children[2].textContent;
+        }
+      }
+    }
+} else { // beta
+    const animeBetaEntries = document.getElementsByClassName("browse-card");
+    for (var i = 0; i < animeBetaEntries.length; i++) {
+      if (animeBetaEntries[i].innerText.indexOf("Score: ") === -1) {
+          const title = animeBetaEntries[i].getElementsByClassName("c-browse-card__title")[0].innerText;
+          animeTitlesToId[title] = "beta";
       }
     }
   }
@@ -129,27 +162,54 @@ function getAnimeTitlesPendingToGetScore() {
 }
 
 function showAnimeScore() {
-  const animeEntries = document.getElementById("main_content").getElementsByClassName("series-title");
-  for (var i = 0; i < animeEntries.length; i++) {
-    chrome.storage.local.get([animeEntries[i].textContent], function(animeScoreAndId) {
-      const id = Object.entries(animeScoreAndId)[0][1].animeId;
-      const score = Object.entries(animeScoreAndId)[0][1].score;
-      const doc = document.getElementById(id);
-      if (doc.children[0].children[0].children[2].textContent.indexOf("| Score: ") === -1) {
-        var textBelowTitle = doc.children[0].children[0].children[2].textContent;
-        textBelowTitle = textBelowTitle.replace(/Ep (\d+).*/, "Ep $1 "); // in the "/anime/updated" endpoint replace the hours ago text in order to show the score
-        doc.children[0].children[0].children[2].textContent = textBelowTitle + "| Score: " + score;
+  const oldCrunchyRollMainContent = document.getElementById("main_content");
+  if (oldCrunchyRollMainContent) {
+    const animeEntries = oldCrunchyRollMainContent.getElementsByClassName("series-title");
+    for (var i = 0; i < animeEntries.length; i++) {
+      chrome.storage.local.get([animeEntries[i].textContent], function(animeScoreAndId) {
+        const id = Object.entries(animeScoreAndId)[0][1].animeId;
+        const score = Object.entries(animeScoreAndId)[0][1].score;
+        const doc = document.getElementById(id);
+        if (doc.children[0].children[0].children[2].textContent.indexOf("| Score: ") === -1) {
+          var textBelowTitle = doc.children[0].children[0].children[2].textContent;
+          textBelowTitle = textBelowTitle.replace(/Ep (\d+).*/, "Ep $1 "); // in the "/anime/updated" endpoint replace the hours ago text in order to show the score
+          doc.children[0].children[0].children[2].textContent = textBelowTitle + "| Score: " + score;
 
-        // fade low score shows:
-        chrome.storage.sync.get({
-          fadeScore: 7.00, // default values
-          fadeOpacity: 0.1
-        }, function(items) {
-          if (parseFloat(score) < parseFloat(items.fadeScore)) {
-            doc.getElementsByTagName('img')[0].style.opacity = parseFloat(items.fadeOpacity);
-          }
-        });        
+          // fade low score shows:
+          chrome.storage.sync.get({
+            fadeScore: 7.00, // default values
+            fadeOpacity: 0.1
+          }, function(items) {
+            if (parseFloat(score) < parseFloat(items.fadeScore)) {
+              doc.getElementsByTagName('img')[0].style.opacity = parseFloat(items.fadeOpacity);
+            }
+          });        
+        }
+      });
+    }
+  } else { // beta
+    console.debug("beta-show");
+    const animeBetaEntries = document.getElementsByClassName("browse-card");
+    for (var i = 0; i < animeBetaEntries.length; i++) {
+      if (animeBetaEntries[i].innerText.indexOf("Score: ") === -1) {
+        const doc = animeBetaEntries[i].getElementsByClassName("c-browse-card__title")[0];
+        const img = animeBetaEntries[i].getElementsByClassName("c-browse-card__poster-wrapper")[0];
+        chrome.storage.local.get([doc.innerText], function(animeScoreAndId) {
+          const score = Object.entries(animeScoreAndId)[0][1].score;
+          var background_color = parseFloat(score) < 5.00 ? "#f00" : parseFloat(score) < 7.50 ? "#fc3" : "#6c3";
+          doc.outerHTML = '<h4 style="text-align: center; background-color: ' + background_color + '"> Score: ' + score + '</h4>' + doc.outerHTML;
+
+          // fade low score shows:
+          chrome.storage.sync.get({
+            fadeScore: 7.00, // default values
+            fadeOpacity: 0.1
+          }, function(items) {
+            if (parseFloat(score) < parseFloat(items.fadeScore)) {
+              img.style.opacity = parseFloat(items.fadeOpacity);
+            }
+          });  
+        });
       }
-    });
+    }
   }
 }
